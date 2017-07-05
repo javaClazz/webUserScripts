a7d8f4fdf4a146fc1de8b807041539d13ccf257d
javaClazz <javadom@163.com>
2017-07-05 16:44:39 +0800

    init project

---
+++ miaopai/miaopaiAutoPlay.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        秒拍视频播放助手v1版
+// @namespace   undefined
+// @version     1.0.0
+// @description 秒拍视频自动播放
+// @author      Anyvone
+// @match       *://*.miaopai.com/show/*
+// @require     https://code.jquery.com/jquery-latest.js
+// @updateURL   https://github.com/javaClazz/webUserScripts/miaopai/miaopaiAutoplay.user.js
+// @downloadURL https://github.com/javaClazz/webUserScripts/miaopai/miaopaiAutoplay.user.js
+// @run-at 		document-start
+// @grant       unsafeWindow
+// @grant       GM_setClipboard
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+alert("hello, my lord!");
